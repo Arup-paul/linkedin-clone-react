e72c96e0863b3e51ec5ed11e288aaf798a7fd80f
Arup paul <aruppaulctg@gmail.com>
2021-06-19 17:03:17 +0600

    home header ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import Login from "./Components/login";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
          <Switch>
              <Route exact path="/">
                  <Login />
+             </Route>
+             <Route exact path="/home">
+                 <Header />
+                 <Home />
              </Route>
          </Switch>
       </Router>
