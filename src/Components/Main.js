dfda80e3071ab66cdc5cb3fb4f5dd8a883114ee0
Arup paul <aruppaulctg@gmail.com>
2021-06-24 16:53:54 +0600

    post modal ui

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,36 @@
 import styled from "styled-components";
+import PostModal from "./PostModal";
+import {useState} from "react";
 
 const Main  = (props) => {
+
+    const [showModal,setShowModal] = useState("close");
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        if(e.target !== e.currentTarget){
+            return;
+        }
+
+        switch (showModal){
+            case "open":
+                setShowModal("close")
+                break;
+            case "close":
+                setShowModal("open");
+                break;
+            default:
+                setShowModal("close");
+                break;
+
+        }
+    }
     return (
         <Container>
            <ShareBox>
                <div>
                    <img src="/images/user.svg" alt=""/>
-                   <button>Start a post</button>
+                   <button onClick={handleClick}>Start a post</button>
                </div>
                <div>
                    <button>
@@ -86,6 +110,7 @@ const Main  = (props) => {
                   </SocialAction>
                 </Article>
             </div>
+            <PostModal showModal={showModal} handleClick={handleClick} />
         </Container>
     );
 }
